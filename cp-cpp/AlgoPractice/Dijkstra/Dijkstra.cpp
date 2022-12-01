4a9323a1d6e7510171ed9de8477cbcf4670a2c11
Md. Najmul Hasan <najmul@qianalysis.com>
2022-12-01 08:57:10 +0600

    Dijkstra done

--- cp-cpp/AlgoPractice/Dijkstra/Dijkstra.cpp
+++ cp-cpp/AlgoPractice/Dijkstra/Dijkstra.cpp
@@ -1,31 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-  
-  return 0;
-}
-
-#include <bits/stdc++.h>
-using namespace std;
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
 
 class IO {
+ public:
+  IO() {
+    // activate fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+  }
+
+  string next() {
+    string str;
+    cin >> str;
+    return str;
+  }
+
+  string nextLine() {
+    string str;
+    getline(cin, str);
+    return str;
+  }
+
   int nextInt() {
-    int a;
-    cin >> a;
+    int n;
+    cin >> n;
+    return n;
+  }
+
+  vector<int> nextInts() { return nextInts(nextInt()); }
+
+  vector<int> nextInts(int n) {
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
     return a;
   }
-};
+
+  void print(string message) { cout << message << endl; }
+
+  void print(vector<int> n, string separator = " ") {
+    if (n.size() == 0) {
+      return;
+    }
+    for (int i = 0; i < n.size() - 1; ++i) cout << n[i] << separator;
+    cout << n[n.size() - 1] << endl;
+  }
+} io;
 
 class Dijkstra {
  private:
+  int vertexCount, edgeCount;
   int graph[10][10];
 
  public:
   void readGraph(string fileName) {
     ifstream fin(fileName);
-    int vertexCount, edgeCount, u, v, w;
+    int u, v, w;
     fin >> vertexCount >> edgeCount;
     for (int i = 0; i < edgeCount; ++i) {
       fin >> u >> v >> w;
@@ -35,13 +67,48 @@ class Dijkstra {
     fin.close();
   }
 
-  vector<int> findShortestPath(int start, int end) {}
+  vector<int> findShortestPath(int start, int end) {
+    vector<bool> visited(vertexCount + 1, false);
+    vector<int> distance(vertexCount + 1, INT_MAX);
+    vector<int> parent(vertexCount + 1, -1);
+
+    distance[start] = 0;
+
+    priority_queue<int> queue;
+    queue.emplace(start);
+
+    while (!queue.empty()) {
+      int currentVertex = queue.top();
+      queue.pop();
+      visited[currentVertex] = true;
+      rep(i, 1, vertexCount + 1) {
+        if (graph[currentVertex][i] == 0) continue;
+        if (visited[i]) continue;
+        if (distance[currentVertex] + graph[currentVertex][i] < distance[i]) {
+          distance[i] = distance[currentVertex] + graph[currentVertex][i];
+          parent[i] = currentVertex;
+          queue.emplace(i);
+        }
+      }
+    }
+
+    vector<int> ans;
+    int last = end;
+    while (parent[last] != -1) {
+      ans.push_back(last);
+      last = parent[last];
+    }
+    ans.push_back(last);
+    // Reverse the vector
+    reverse(ans.begin(), ans.end());
+    return ans;
+  }
 };
 
 int main(int argc, char const *argv[]) {
   Dijkstra dijkstra;
   dijkstra.readGraph("in.txt");
-  vector<int> path = dijkstra.findShortestPath(1, 5);
+  vector<int> path = dijkstra.findShortestPath(1, 6);
   for (int i = 0; i < path.size() - 1; ++i) {
     cout << path[i] << ", ";
   }

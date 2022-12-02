3b7d6849d1796a0c71cbe9b8876ab5e53927f6ae
Md. Najmul Hasan <najmul@qianalysis.com>
2022-12-02 12:06:11 +0600

    Some more macro ans snippets

--- cp-cpp/templates/Test.cpp
+++ cp-cpp/templates/Test.cpp
@@ -3,42 +3,16 @@ using namespace std;
 
 #define rep(i, a, b) for (auto i = (a); i < (b); ++i)
 
-struct IO {
-  string next() {
-    string str;
-    cin >> str;
-    return str;
-  }
+void solve() {}
 
-  string nextLine() {
-    string str;
-    getline(cin, str);
-    return str;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int testCount = 1;
+  // testCount = io.nextInt();
+  while (testCount > 0) {
+    solve();
+    --testCount;
   }
-
-  int nextInt() {
-    int n;
-    cin >> n;
-    return n;
-  }
-
-  vector<int> nextInts() {
-    int n;
-    cin >> n;
-    return nextInts(n);
-  }
-
-  vector<int> nextInts(int n) {
-    vector<int> a(n);
-    rep(i, 0, n) cin >> a[i];
-    return a;
-  }
-
-  void print(vector<int> n, string separator = " ") {
-    if (n.size() == 0) return;
-    rep(i, 0, n.size() - 1) cout << n[i] << separator;
-    cout << n[n.size() - 1] << endl;
-  }
-} io;
-
-int main(int argc, char const *argv[]) { return 0; }
+  return 0;
+}
